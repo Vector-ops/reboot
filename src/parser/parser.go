3bf5426d5a9b3cf440a8418bb9e794ccb82bf184
Vector-ops <sumith2827@gmail.com>
2024-05-20 18:45:18 +0530

    feat: implement recursive descent parser
    - implement parsesr for let statements

---
+++ src/parser/parser.go
@@ -0,0 +1,129 @@
+package parser
+
+import (
+	"fmt"
+	"reboot/ast"
+	"reboot/lexer"
+	"reboot/token"
+)
+
+type Parser struct {
+	l *lexer.Lexer
+
+	errors []string
+
+	curToken  token.Token
+	peekToken token.Token
+}
+
+// New function takes an instance of
+// lexer and returns a Parser
+func New(l *lexer.Lexer) *Parser {
+	p := &Parser{
+		l:      l,
+		errors: []string{},
+	}
+	p.nextToken()
+	p.nextToken()
+
+	return p
+}
+
+// Errors method returns a slice of errors
+// encountered while parsing the tokens
+func (p *Parser) Errors() []string {
+	return p.errors
+}
+
+func (p *Parser) peekError(t token.TokenType) {
+	msg := fmt.Sprintf("expected next token to be %s, got %s instead", t, p.peekToken.Type)
+
+	p.errors = append(p.errors, msg)
+}
+
+// nextToken method sets the
+// current token to the peek token
+// and advances the peek token
+func (p *Parser) nextToken() {
+	p.curToken = p.peekToken
+	p.peekToken = p.l.NextToken()
+}
+
+// ParseProgram method parses the tokens
+// returned by the lexer, appends the statements
+// to the Statements slice and returns a pointer
+// to the root node of the AST
+func (p *Parser) ParseProgram() *ast.Program {
+	program := &ast.Program{}
+	program.Statements = []ast.Statement{}
+
+	for !p.curTokenIs(token.EOF) {
+		stmt := p.parseStatement()
+		if stmt != nil {
+			program.Statements = append(program.Statements, stmt)
+		}
+		p.nextToken()
+	}
+
+	return program
+}
+
+// parseStatement method calls the respective
+// statement parser method based on the token
+// and returns a statement object
+func (p *Parser) parseStatement() ast.Statement {
+	switch p.curToken.Type {
+	case token.LET:
+		return p.parseLetStatement()
+	default:
+		return nil
+	}
+}
+
+// parseLetStatement method constructs an ast.LetStatement
+// node with the token.Let token.
+func (p *Parser) parseLetStatement() *ast.LetStatement {
+	stmt := &ast.LetStatement{Token: p.curToken}
+
+	if !p.expectPeek(token.IDENT) {
+		return nil
+	}
+
+	stmt.Name = &ast.Identifier{Token: p.curToken, Value: p.curToken.Literal}
+
+	if !p.expectPeek(token.ASSIGN) {
+		return nil
+	}
+
+	for !p.curTokenIs(token.SEMICOLON) {
+		p.nextToken()
+	}
+
+	return stmt
+}
+
+// curTokenIs checks if the current token matches
+// the passed token type.
+func (p *Parser) curTokenIs(t token.TokenType) bool {
+	return p.curToken.Type == t
+}
+
+// peekTokenIs method checks if the next token type
+// matches the passed token type.
+func (p *Parser) peekTokenIs(t token.TokenType) bool {
+	return p.peekToken.Type == t
+}
+
+// expectPeek method checks if the next token matches
+// the passed token type. It moves the parser to the
+// next token if the types match and returns true
+// otherwise returns false.
+func (p *Parser) expectPeek(t token.TokenType) bool {
+	if p.peekTokenIs(t) {
+		p.nextToken()
+		return true
+	} else {
+		p.peekError(t)
+		return false
+	}
+}
