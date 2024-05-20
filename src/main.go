3a315016aaed72e1b6b59a32039ddc70ecec3216
Vector-ops <sumith2827@gmail.com>
2024-05-20 15:39:35 +0530

    feat: add REPL

--- src/main.go
+++ src/main.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"os"
+	"os/user"
+	"reboot/repl"
+)
 
+func main() {
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Hello %s! This is Reboot programming language!\n", user.Username)
+	fmt.Printf("Feel free to type in commands\n")
+	repl.Start(os.Stdin, os.Stdout)
 }
