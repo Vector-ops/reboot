a4945a2fe7b37226b58802cc9ff8bb8e016c3e10
Vector-ops <sumith2827@gmail.com>
2024-05-29 21:26:48 +0530

    feat: implement runner

--- src/main.go
+++ src/main.go
@@ -5,14 +5,28 @@ import (
 	"os"
 	"os/user"
 	"reboot/repl"
+	"reboot/runner"
+	"strings"
 )
 
 func main() {
-	user, err := user.Current()
-	if err != nil {
-		panic(err)
+
+	if len(os.Args) < 2 {
+		user, err := user.Current()
+		if err != nil {
+			panic(err)
+		}
+		fmt.Printf("Hello %s! This is Reboot programming language!\n", user.Username)
+		fmt.Printf("Feel free to type in commands\n")
+		repl.Start(os.Stdin, os.Stdout)
+
+		return
 	}
-	fmt.Printf("Hello %s! This is Reboot programming language!\n", user.Username)
-	fmt.Printf("Feel free to type in commands\n")
-	repl.Start(os.Stdin, os.Stdout)
+	pathToFile := os.Args[1]
+
+	if strings.Split(pathToFile, ".")[1] != "re" {
+		panic("invalid file")
+	}
+	runner.Run(pathToFile)
+
 }
